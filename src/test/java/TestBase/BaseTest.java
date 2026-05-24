22af6737cd7335e84065183fea15aac6a9de8ae4
pranitBramhankar <pranitsb1997@gmail.com>
2026-05-25 00:11:09 +0530

    Adding Extent report manager

--- src/test/java/TestBase/BaseTest.java
+++ src/test/java/TestBase/BaseTest.java
@@ -1,9 +1,15 @@
 package TestBase;
 
+import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.Properties;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -47,6 +53,22 @@ public class BaseTest {
 		driver.get(properties.getProperty("ApplicationURL"));
 
 	}
+	
+	public String captureScreen(String tname) throws IOException {
+
+		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+				
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		
+		String targetFilePath=System.getProperty("user.dir")+"\\screenshots\\" + tname + "_" + timeStamp + ".png";
+		File targetFile=new File(targetFilePath);
+		
+		sourceFile.renameTo(targetFile);
+			
+		return targetFilePath;
+
+	}	
 	
 	@AfterMethod
 	public void tearDownAllSessions() {
